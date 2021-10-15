eab888cae25d2dd7d65b859e267c10da287a6a96
DarkAthena <darkathena@qq.com>
2021-10-15 21:06:53 +0800

    Update list_table_sql.py

--- list_table_sql.py
+++ list_table_sql.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+#coding=utf-8
+#功能 ：获取sql中的所有表和视图名
+#日期 ：2021-10-15 
+#作者：Dark-Athena
+#EMAIL:darkathena@qq.com
+"""
+Copyright DarkAthena(darkathena@qq.com)
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 import os
 import re,json
 from antlr4 import *
